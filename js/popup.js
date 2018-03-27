3b7ba3cd9e177d339d416c855b8b76c51ba7c6e5
Artyom <tema-luch@mail.ru>
2018-03-28 00:46:45 +1000

    Добавил Js и дополнительные критерии

---
+++ js/popup.js
@@ -0,0 +1,58 @@
+var search = document.querySelector(".hotel-search");
+
+var popup = document.querySelector(".hotel-search-modal");
+
+  var form = popup.querySelector("form");
+  var arrival = popup.querySelector("[name=arrival]");
+  var departure = popup.querySelector("[name=departure]");
+  var adult = popup.querySelector("[name=adult]");
+  var child = popup.querySelector("[name=child]");
+
+  var isStorageSupport = true;
+  var storageAdult = "";
+  var storageChild = "";
+
+  try {
+    storageAdult = localStorage.getItem("adult");
+    storageChild = localStorage.getItem("child");
+  }
+  catch(err) {
+    isStorageSupport = false;
+  }
+
+  search.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.toggle("modal-show");
+    if (storageAdult) {
+      adult.value = storageAdult;
+    }
+    if (storageChild) {
+      child.value = storageChild;
+    }
+    arrival.focus();
+  });
+
+  form.addEventListener("submit", function(evt) {
+    if (!arrival.value || !departure.value || !adult.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+    else {
+      if (isStorageSupport) {
+        localStorage.setItem("adult", adult.value);
+        localStorage.setItem("child", child.value);
+        }
+    }
+  });
+
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+
